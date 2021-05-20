bf48fc331050a3c4d79ee47ff2321a575b18d269
ABDALLAH Rasha <rabdallah@murex.com>
2021-05-20 20:56:45 +0200

    adding code for refactoring kata

---
+++ test/account/AccountCreationServiceTest.java
@@ -0,0 +1,13 @@
+package account;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AccountCreationServiceTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertEquals(2, 2);
+    }
+}
